0c17eb733eeb1f9c935fa68781be46cf47a0e14c
jeffersonfrutuoso <jeffersonfrutuoso413@gmail.com>
2025-08-01 23:12:21 -0300

    feat: add rabbitMq

---
+++ email/src/main/java/dev/frutuoso/email/config/RabbitMq.java
@@ -0,0 +1,14 @@
+package dev.frutuoso.email.config;
+import org.springframework.amqp.core.Queue;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class RabbitMq {
+    private final String queueName = "email-queue";
+
+    @Bean
+    public Queue queue(){
+        return new Queue(queueName, true);
+    }
+}
---
+++ email/src/main/java/dev/frutuoso/email/consumer/EmailConsumer.java
@@ -0,0 +1,13 @@
+package dev.frutuoso.email.consumer;
+import org.springframework.amqp.rabbit.annotation.RabbitListener;
+import org.springframework.messaging.handler.annotation.Payload;
+import org.springframework.stereotype.Component;
+
+@Component
+public class EmailConsumer {
+
+    @RabbitListener(queues = "email-queue")
+    public void listenEmailQueue(@Payload String string){
+        System.out.println(string);
+    }
+}
