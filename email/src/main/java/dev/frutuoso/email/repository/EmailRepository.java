fb51041fb123cb18b5be3c619677e44d3ea09982
jeffersonfrutuoso <jeffersonfrutuoso413@gmail.com>
2025-08-01 23:14:12 -0300

    feat: add email repository

---
+++ email/src/main/java/dev/frutuoso/email/repository/EmailRepository.java
@@ -0,0 +1,9 @@
+package dev.frutuoso.email.repository;
+
+import dev.frutuoso.email.domain.EmailModel;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.UUID;
+
+public interface EmailRepository extends JpaRepository<EmailModel, UUID> {
+}
