b5162e35885384213e2567316eed338e46ddd996
jeffersonfrutuoso <jeffersonfrutuoso413@gmail.com>
2025-08-12 19:03:44 -0300

    feat: add user repository

---
+++ user/src/main/java/dev/frutuoso/user/repositoy/UserRepository.java
@@ -0,0 +1,11 @@
+package dev.frutuoso.user.repositoy;
+
+import dev.frutuoso.user.domain.UserModel;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.UUID;
+
+@Repository
+public interface UserRepository extends JpaRepository<UserModel, UUID> {
+}
